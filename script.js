579a1773f5644df4851219d5174c09f6fcf01691
Razak Adli Tama <76839067+rzkadltm@users.noreply.github.com>
2023-12-08 20:13:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let display = document.getElementById("displays");
+
+function munculinAngka(value) {
+  // Code Here
+}
+
+function bersihkanAngka() {
+  // Code here
+}
+
+function hitungHasil() {
+  // Code here
+}
